82cdba49d4046346ead2aabfbae96cc9e05e55ad
LisitsynP <lis.28_93@mail.ru>
2025-01-12 10:25:49 +0200

    AppSider.jsx component layout completed

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,16 +1,106 @@
-import { Layout } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import { Layout, Card, Statistic, List, Spin, Typography, Tag } from "antd";
+import { useEffect, useState } from "react";
+import { fakeFetchCrypto, FetchAssets } from "../../api";
+import { percentDifference, capitalize } from "../../utils";
 
 const siderStyle = {
-  textAlign: "center",
-  lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#1677ff",
+  padding: "1rem",
 };
 
 export default function AppSider() {
+  const [loading, setLoading] = useState(false);
+  const [crypto, setCrypto] = useState([]);
+  const [assets, setAssets] = useState([]);
+  useEffect(() => {
+    async function preloader() {
+      setLoading(true);
+      const { result } = await fakeFetchCrypto();
+      const assets = await FetchAssets();
+
+      setAssets(
+        assets.map((asset) => {
+          const coin = result.find((c) => c.id === asset.id);
+          return {
+            icon: coin.icon,
+            grow: asset.price < coin.price,
+            growPercent: percentDifference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: coin.price * asset.amount - asset.price * asset.amount,
+            ...asset,
+          };
+        })
+      );
+      setCrypto(result);
+      setLoading(false);
+    }
+    preloader();
+  }, []);
+
+  if (loading) {
+    return <Spin fullscreen />;
+  }
+
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-      Sider
+      {assets.map((asset) => {
+        return (
+          <Card key={asset.id} style={{ marginBottom: "1rem" }}>
+            <Statistic
+              // title={capitalize(asset.id)}
+              title={
+                <div>
+                  <img
+                    style={{ width: "20px", marginRight: "10px" }}
+                    src={asset.icon}
+                    alt={asset.id}
+                  />
+                  {capitalize(asset.id)}
+                </div>
+              }
+              value={asset.totalAmount}
+              precision={2}
+              valueStyle={{
+                color: asset.grow ? "#3f8600" : "#cf1322",
+              }}
+              prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+              suffix="$"
+            />
+            <List
+              size="small"
+              dataSource={[
+                {
+                  title: "Total Profit",
+                  value: asset.totalProfit,
+                  withTag: true,
+                },
+                { title: "Asset Amount", value: asset.amount, isPlain: true },
+                // { title: "Difference", value: asset.growPercent },
+              ]}
+              renderItem={(item) => (
+                <List.Item>
+                  <span>{item.title}</span>
+                  <span>
+                    {item.isPlain && item.value}
+
+                    {item.withTag && (
+                      <Tag color={asset.grow ? "green" : "red"}>
+                        {asset.growPercent}%
+                      </Tag>
+                    )}
+
+                    {!item.isPlain && (
+                      <Typography.Text type={asset.grow ? "success" : "danger"}>
+                        {item.value.toFixed(2)}$
+                      </Typography.Text>
+                    )}
+                  </span>
+                </List.Item>
+              )}
+            />
+          </Card>
+        );
+      })}
     </Layout.Sider>
   );
 }
